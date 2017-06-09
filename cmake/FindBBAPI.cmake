eb3c90a434c69e53e05d975a29fd6bad88e7b66e
Elsa Gonsiorowski <gonsie@llnl.gov>
2017-06-09 16:14:42 -0700

    Find BBAPI for cmake

---
+++ cmake/FindBBAPI.cmake
@@ -0,0 +1,31 @@
+# - Try to find libbbAPI
+# Once done this will define
+#  BBAPI_FOUND - System has libdatawarp
+#  BBAPI_INCLUDE_DIRS - The libdatawarp include directories
+#  BBAPI_LIBRARIES - The libraries needed to use libdatawarp
+
+FIND_PATH(WITH_BBAPI_PREFIX
+    NAMES include/bbapi.h
+)
+
+FIND_LIBRARY(BBAPI_LIBRARIES
+    NAMES bbAPI
+    HINTS ${WITH_BBAPI_PREFIX}/lib
+)
+
+FIND_PATH(BBAPI_INCLUDE_DIRS
+    NAMES bbapi.h
+    HINTS ${WITH_BBAPI_PREFIX}/include
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(BBAPI DEFAULT_MSG
+    BBAPI_LIBRARIES
+    BBAPI_INCLUDE_DIRS
+)
+
+# Hide these vars from ccmake GUI
+MARK_AS_ADVANCED(
+	BBAPI_LIBRARIES
+	BBAPI_INCLUDE_DIRS
+)
