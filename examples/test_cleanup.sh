26574768e1203232d3d36c722377c47d799cd088
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2017-07-25 10:35:43 -0700

    testing config and cleanup script

---
+++ examples/test_cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+# SCR cleanup script
+# Usage:
+# ./cleanup.sh [file]
+# where file contains a list of items to remove
+
+if [ $# -gt 1 ]; then
+	echo "Usage: $0 [output]"
+	exit 1
+fi
+
+rm -rf /tmp/$USER/scr.*/
+rm -rf .scr/
+if [ $# -eq 1 ]; then
+	rm -rf $1
+fi
+
+exit 0
