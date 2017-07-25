9abe0b64206f93e9c429045b72747a599f8cc953
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2017-07-25 15:09:52 -0700

    naming things cleanup

--- cmake/SCR_ADD_TEST.cmake
+++ cmake/SCR_ADD_TEST.cmake
@@ -4,10 +4,10 @@ FUNCTION(SCR_ADD_TEST name args outputs)
 
 	ADD_TEST(NAME ${name}_restart COMMAND ./${name} ${args})
 	SET_PROPERTY(TEST ${name}_restart APPEND PROPERTY ENVIRONMENT "SCR_CONF_FILE=${CMAKE_CURRENT_SOURCE_DIR}/test.conf")
-	SET_PROPERTY(TEST ${name}_restart APPEND PROPERTY DEPENDS start_${name})
+	SET_PROPERTY(TEST ${name}_restart APPEND PROPERTY DEPENDS ${name}_start)
 
 	# Note: cleanup scripts automatically cleans /tmp and .scr
-	ADD_TEST(NAME ${name}_cleanup COMMAND ./cleanup.sh ${outputs} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-	SET_PROPERTY(TEST ${name}_cleanup APPEND PROPERTY DEPENDS restart_${name})
+	ADD_TEST(NAME ${name}_cleanup COMMAND ./test_cleanup.sh ${outputs} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+	SET_PROPERTY(TEST ${name}_cleanup APPEND PROPERTY DEPENDS ${name}_restart)
 
 ENDFUNCTION(SCR_ADD_TEST name args outputs)
