427fdb61e9e4bebc5aff35b6824eb2a3bf2e0b76
cstanavige <cameronstanavige@gmail.com>
2017-08-11 09:53:38 -0700

    Switched serial tests to use srun on single node. May have violated some good coding practices.parallel_test_interpose_multiple_start/restart still seems to fail randomly when timesteps do not agree.

--- cmake/SCR_ADD_TEST.cmake
+++ cmake/SCR_ADD_TEST.cmake
@@ -1,9 +1,17 @@
 FUNCTION(SCR_ADD_TEST name args outputs)
+
+  SET(s_nodes "1")
+  IF(${SCR_RESOURCE_MANAGER} STREQUAL "NONE")
+    SET(test_param mpirun -np ${s_nodes})
+  ELSEIF(${SCR_RESOURCE_MANAGER} STREQUAL "SLURM")
+    SET(test_param srun -ppdebug -t 5 -N ${s_nodes} ) 
+  ENDIF(${SCR_RESOURCE_MANAGER} STREQUAL "NONE")
+
 	# Serial Tests
-	ADD_TEST(NAME serial_${name}_start COMMAND ./${name} ${args})
+	ADD_TEST(NAME serial_${name}_start COMMAND ${test_param} ./${name} ${args})
 	SET_PROPERTY(TEST serial_${name}_start APPEND PROPERTY ENVIRONMENT "SCR_CONF_FILE=${CMAKE_CURRENT_SOURCE_DIR}/test.conf")
 
-	ADD_TEST(NAME serial_${name}_restart COMMAND ./${name} ${args})
+	ADD_TEST(NAME serial_${name}_restart COMMAND ${test_param} ./${name} ${args})
 	SET_PROPERTY(TEST serial_${name}_restart APPEND PROPERTY ENVIRONMENT "SCR_CONF_FILE=${CMAKE_CURRENT_SOURCE_DIR}/test.conf")
 	SET_PROPERTY(TEST serial_${name}_restart APPEND PROPERTY DEPENDS serial_${name}_start)
 
@@ -17,11 +25,11 @@ FUNCTION(SCR_ADD_TEST name args outputs)
 	SET_PROPERTY(TEST serial_${name}_cleanup APPEND PROPERTY DEPENDS serial_${name}_restart)
 
 	# Parallel Tests
-	SET(nodes "4")
+	SET(p_nodes "4")
 	IF(${SCR_RESOURCE_MANAGER} STREQUAL "NONE")
-		SET(test_param mpirun -np ${nodes})
+		SET(test_param mpirun -np ${p_nodes})
 	ELSEIF(${SCR_RESOURCE_MANAGER} STREQUAL "SLURM")
-		SET(test_param srun -ppdebug -t 5 -N ${nodes} -n ${nodes})
+		SET(test_param srun -ppdebug -t 5 -N ${p_nodes} -n ${p_nodes})
 	ENDIF(${SCR_RESOURCE_MANAGER} STREQUAL "NONE")
 
 	ADD_TEST(NAME parallel_${name}_start COMMAND ${test_param} ./${name} ${args})
