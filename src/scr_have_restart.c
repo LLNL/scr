e4694ea27dbdc04768eda01bbfe4346f05b4335d
Michael M Pozulp <pozulp1@llnl.gov>
2017-09-06 14:50:23 -0700

    Update scr_have_restart with error checking and use flag from SCR_Have_restart instead of return code to decide if a restart was found

--- src/scr_have_restart.c
+++ src/scr_have_restart.c
@@ -1,4 +1,16 @@
-// scr_have_restart.c
+/*
+ * Copyright (c) 2009, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Written by Adam Moody <moody20@llnl.gov>.
+ * LLNL-CODE-411039.
+ * All rights reserved.
+ * This file is part of The Scalable Checkpoint / Restart (SCR) library.
+ * For details, see https://sourceforge.net/projects/scalablecr/
+ * Please also read this file: LICENSE.TXT.
+*/
+
+/* Utility to print the name of the latest restart */
+
 #include "scr.h"
 #include "mpi.h"
 #include <stdio.h>
@@ -6,18 +18,32 @@
 int main(int *argc, char ***argv)
 {
     char fname[SCR_MAX_FILENAME];
+    char *myname  = "scr_have_restart";
     int flag;
     int rank;
-    int retval = SCR_FAILURE;
+    int rc;
 
-    // TODO: add error checking to all MPI and SCR calls
-    MPI_Init(NULL, NULL);
-    SCR_Init();
-    retval = SCR_Have_restart(&flag, fname);
+    rc = MPI_Init(NULL, NULL);
+    if (rc != MPI_SUCCESS) {
+        fprintf(stderr, "%s: MPI_Init failed %d", myname, rc);
+    }
+    
+    rc = SCR_Init();
+    if (rc != SCR_SUCCESS) {
+        fprintf(stderr, "%s: SCR_Init failed %d", myname, rc);
+    }
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if ( (rank == 0) && (retval == SCR_SUCCESS) )
-    {
+    rc = SCR_Have_restart(&flag, fname);
+    if (rc != SCR_SUCCESS) {
+        fprintf(stderr, "%s: SCR_Have_restart failed %d", myname, rc);
+    }
+
+    rc = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rc != MPI_SUCCESS) {
+        fprintf(stderr, "%s: MPI_Comm_rank failed %d", myname, rc);
+    }
+
+    if ( (rank == 0) && (flag > 0) ) {
         printf("%s\n", fname);
     }
 }
--- src/scr_util.h
+++ src/scr_util.h
@@ -33,6 +33,8 @@
 #ifndef SCR_UTIL_H
 #define SCR_UTIL_H
 
+#define SCR_FAILURE (1)
+
 #include <stdlib.h>
 #include <stdint.h>
 
