0d2de9ae1283e1cbe024c1e28fcd7b99c1c115fa
Roland Haas <rhaas@illinois.edu>
2020-05-26 15:38:38 -0500

    Revert "SCR: run tests without using test.conf"
    This reverts commit 4b03485aac2d5a3f6969d14014290218d6effc3b.

--- examples/test_api.c
+++ examples/test_api.c
@@ -426,10 +426,6 @@ int main (int argc, char* argv[])
   MPI_Barrier(MPI_COMM_WORLD);
   double init_start = MPI_Wtime();
   if (use_scr) {
-    SCR_Config("STORE=/dev/shm GROUP=NODE COUNT=1");
-    SCR_Config("SCR_COPY_TYPE=FILE");
-    SCR_Config("CKPT=0 INTERVAL=1 GROUP=NODE STORE=/dev/shm TYPE=XOR SET_SIZE=16");
-    SCR_Config("SCR_DEBUG=1");
     if (SCR_Init() != SCR_SUCCESS){
       printf("Failed initializing SCR\n");
       return 1;
