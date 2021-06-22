2b969df4ca563b6b1ac56604d9a717efafb0b1f6
chaseleif <chaseleif@icloud.com>
2021-06-22 00:35:20 -0500

    SLURM second pass completed. Next is expanding launcher classes

---
+++ scripts/pyfe/pyfe/scr_joblauncher_jsrun.py
@@ -0,0 +1,15 @@
+#! /usr/env python
+
+# scr_joblauncher_jsrun.py
+# The SCR_Joblauncher_jsrun class provides interpretation for the jsrun launcher
+
+from scr_joblauncher_base import SCR_Joblauncher_Base
+
+class SCR_Joblauncher_srun(SCR_Joblauncher_Base):
+  def __init__(self):
+    self.launcher = 'jsrun'
+    self.use_scr_watchdog = 0 # SCR_WATCHDOG not supported on LSF
+    self.nopargv = None
+    self.scr_end_time = [] # LSF/scr_env.in:181
+    self.excludeargs = '--exclude_hosts=$down_nodes'
+    #super(SCR_Joblauncher_srun, self).__init__()
--- scripts/pyfe/pyfe/scr_joblauncher_srun.py
+++ scripts/pyfe/pyfe/scr_joblauncher_srun.py
@@ -7,5 +7,13 @@ from scr_joblauncher_base import SCR_Joblauncher_Base
 
 class SCR_Joblauncher_srun(SCR_Joblauncher_Base):
   def __init__(self):
-    pass
+    self.launcher = 'srun'
+    self.use_scr_watchdog=1
+    self.nopargv = ['srun','/bin/hostname']
+    self.scr_end_time = []
+    self.scr_end_time.append(['scontrol', '--oneliner','show','job','$SLURM_JOBID'])
+    self.scr_end_time.append(['perl','-n','-e','\'m/EndTime=(\\S*)/ and print $1\''])
+    self.scr_end_time.append(['date','-d'])
+    self.excludeargs = '--exclude $down_nodes'
+    
     #super(SCR_Joblauncher_srun, self).__init__()
