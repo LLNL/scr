08a8e80e389eb14ffd76cd0a843fcd149c6b49f1
chaseleif <chaseleif@icloud.com>
2021-06-24 13:11:32 -0500

    moving resource manager out of env

--- scripts/pyfe/pyfe/resmgr/scr_resourcemgr.py
+++ scripts/pyfe/pyfe/resmgr/scr_resourcemgr.py
@@ -3,13 +3,19 @@
 # scr_resourcemgr.py
 
 # SCR_ResourceMgr is class constructor for the specific resource managers
+# the SCR_ResourceMgr constructor takes a resmgr argument
+# resmgr can be: 'SLURM', 'LSF', 'APRUN', 'PMIX', None
+# if resmgr is None we check scr_const.SCR_RESOURCE_MANAGER
+# returns appropriate resource manager class according to resmgr value
+# returns an SCR_ResourceMgr_Base object if the resource manager was not determined
 
 from pyfe import scr_const
 
 from pyfe.resmgr.scr_resourcemgr_base import SCR_ResourceMgr_Base
-from pyfe.resmgr.scr_resourcemgr_srun import SCR_ResourceMgr_srun
-from pyfe.resmgr.scr_resourcemgr_jsrun import SCR_ResourceMgr_jsrun
-from pyfe.resmgr.scr_resourcemgr_mpirun import SCR_ResourceMgr_mpirun
+from pyfe.resmgr.scr_resourcemgr_slurm import SCR_ResourceMgr_SLURM
+from pyfe.resmgr.scr_resourcemgr_lsf import SCR_ResourceMgr_LSF
+from pyfe.resmgr.scr_resourcemgr_aprun import SCR_ResourceMgr_APRUN
+from pyfe.resmgr.scr_resourcemgr_pmix import SCR_ResourceMgr_PMIX
 
 class SCR_ResourceMgr:
   def __new__(cls,resmgr=None):
---
+++ scripts/pyfe/pyfe/scr_env.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+# scr_env.py
+# SCR_Env contains general values from the environment
+
+import os
+from pyfe import scr_const
+from pyfe.scr_common import scr_prefix
+
+class SCR_Env:
+  def __init__(self,env=None):
+    # should this still be set here ?
+    # it could be changed later... not certain this will be needed though
+    #if env is None:
+    #  env = scr_const.SCR_RESOURCE_MANAGER
+    # we can keep a reference to the launcher and resource manager
+    self.launcher = None
+    self.resmgr = None
+    # initialize the infos
+    self.conf = {}
+    #self.conf['env'] = env
+    self.conf['prefix'] = scr_prefix()
+    self.conf['nodes_file'] = scr_const.X_BINDIR+'/scr_nodes_file'
+    self.conf['user'] = os.environ.get('USER')
+    #self.conf['jobid'] = self.getjobid()
+    #self.conf['nodes'] = self.get_job_nodes()
+
+  # set the prefix
+  def set_prefix(self,prefix):
+    self.conf['prefix'] = prefix
+
+if __name__ == '__main__':
+  scr_env = SCR_Env()
+  for attr in dir(scr_env):
+    if attr.startswith('__'):
+      continue
+    thing = getattr(scr_env,attr)
+    if type(thing) is dict:
+      for key in thing:
+        print('scr_env.'+attr+'['+key+'] = '+str(thing[key]))
+    else:
+      print('scr_env.'+attr+' = '+str(thing))
