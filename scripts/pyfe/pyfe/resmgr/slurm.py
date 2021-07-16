dbf4272a022e459dd90011c45f318989de5a53a6
Adam Moody <moody20@llnl.gov>
2021-07-16 15:05:24 -0700

    parse endtime from slurm

--- scripts/pyfe/pyfe/resmgr/slurm.py
+++ scripts/pyfe/pyfe/resmgr/slurm.py
@@ -4,6 +4,8 @@
 # SLURM is a subclass of ResourceManager
 
 import os, re
+import datetime
+
 from pyfe import scr_const
 from pyfe.scr_common import runproc, pipeproc
 from pyfe.resmgr import nodetests, ResourceManager
@@ -15,23 +17,24 @@ class SLURM(ResourceManager):
   def __init__(self):
     super(SLURM, self).__init__(resmgr='SLURM')
 
-  # get job id, setting environment flag here
+  # get SLURM jobid of current allocation
   def getjobid(self):
-    if self.conf['jobid'] is not None:
-      return self.conf['jobid']
     return os.environ.get('SLURM_JOBID')
 
-  def get_jobstep_id(self,user='',pid=-1):
-    if user=='' or self.conf['jobid'] is None:
+  # query SLURM for most recent jobstep in current allocation
+  def get_jobstep_id(self, user='', pid=-1):
+    jobid = self.getjobid()
+    if user == '' or jobid is None:
       return -1
+
     # get job steps for this user and job, order by decreasing job step
     # so first one should be the one we are looking for
+    #   squeue -h -s -u $user -j $jobid -S "-i"
     # -h means print no header, so just the data in this order:
     # STEPID         NAME PARTITION     USER      TIME NODELIST
-    cmd = ['squeue','-h','-s','-u',user,'-j',str(self.conf['jobid']),'-S','\"-i\"']
-    # my $cmd="squeue -h -s -u $user -j $jobid -S \"-i\"";
-    output = runproc(argv=cmd,getstdout=True)[0]
-    output = re.search('\d+',output)
+    cmd = ['squeue', '-h', '-s', '-u', user, '-j', jobid, '-S', '\"-i\"']
+    output = runproc(argv=cmd, getstdout=True)[0]
+    output = re.search('\d+', output)
     if output is None:
       return -1
     return output[0]
@@ -40,14 +43,14 @@ class SLURM(ResourceManager):
   def get_job_nodes(self):
     return os.environ.get('SLURM_NODELIST')
 
+  # use sinfo to query SLURM for the list of nodes it thinks to be down
   def get_downnodes(self):
-    val = os.environ.get('SLURM_NODELIST')
-    if val is not None:
-      argv = ['sinfo','-ho','%N','-t','down','-n',val]
-      down, returncode = runproc(argv=argv,getstdout=True)
+    nodelist = self.get_job_nodes()
+    if nodelist is not None:
+      argv = ['sinfo', '-ho', '%N', '-t', 'down', '-n', nodelist]
+      down, returncode = runproc(argv=argv, getstdout=True)
       if returncode == 0:
         down = down.strip()
-        self.conf['down'] = down
         return down
     return None
 
@@ -57,18 +60,25 @@ class SLURM(ResourceManager):
       return 1
     return runproc(argv=['scancel',str(jobid)])[1]
 
+  # query SLURM for allocation endtime, expressed as secs since epoch
   def get_scr_end_time(self):
-    if self.conf['jobid'] is None:
-      return None
-    argv = []
-    argv.append(['scontrol','--oneliner','show','job',self.conf['jobid']])
-    argv.append(['perl','-n','-e','\'m/EndTime=(\\S*)/ and print $1\''])
-    output = pipeproc(argvs=argv,getstdout=True)[0]
-    argv = ['date','-d',output.rstrip()]
-    output = runproc(argv=argv,getstdout=True)[0].strip()
-    if output.isdigit():
-      return int(output)
-    return 0
+    # get jobid
+    jobid = self.getjobid()
+    if jobid is None:
+      return 0
+
+    # ask scontrol for endtime of this job
+    argv = ['scontrol', '--oneliner', 'show', 'job', jobid]
+    output = runproc(argv=argv, getstdout=True)[0]
+    m = re.search('EndTime=(\\S*)', output)
+    if not m:
+      return 0
+
+    # parse time string like "2021-07-16T14:05:12" into secs since epoch
+    timestr = m.group(1)
+    dt = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S")
+    timestamp = int(dt.strftime("%s"))
+    return timestamp
 
   # return a hash to define all unavailable (down or excluded) nodes and reason
   def list_down_nodes_with_reason(self,nodes=[], scr_env=None, free=False, cntldir_string=None, cachedir_string=None):
