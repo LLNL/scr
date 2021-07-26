189dcfac415b810dbd58ad871f83b63b97bc29cb
Adam Moody <moody20@llnl.gov>
2021-07-26 16:20:58 -0700

    use runproc verbose option from cli wrappers

--- scripts/pyfe/pyfe/cli/scr_flush_file.py
+++ scripts/pyfe/pyfe/cli/scr_flush_file.py
@@ -7,14 +7,15 @@ from pyfe.scr_common import runproc
 
 
 class SCRFlushFile:
-  def __init__(self, prefix):
+  def __init__(self, prefix, verbose=False):
     self.bindir = scr_const.X_BINDIR  # path to SCR bin directory
     self.prefix = prefix  # path to SCR_PREFIX
+    self.verbose = verbose
     self.exe = os.path.join(self.bindir, "scr_flush_file") + " --dir " + str(prefix)
 
   # return list of output datasets
   def list_dsets_output(self):
-    dsets, rc = runproc(self.exe + " --list-output", getstdout=True)
+    dsets, rc = runproc(self.exe + " --list-output", getstdout=True, verbose=self.verbose)
     if rc == 0:
       return dsets.split()
     return []
@@ -24,40 +25,40 @@ class SCRFlushFile:
     cmd = self.exe + " --list-ckpt"
     if before:
       cmd = cmd + " --before " + str(before)
-    dsets, rc = runproc(cmd, getstdout=True)
+    dsets, rc = runproc(cmd, getstdout=True, verbose=self.verbose)
     if rc == 0:
       return dsets.split()
     return []
 
   # determine whether this dataset needs to be flushed
   def need_flush(self, d):
-    rc = runproc(self.exe + " --need-flush " + str(d))[1]
+    rc = runproc(self.exe + " --need-flush " + str(d), verbose=self.verbose)[1]
     return (rc == 0)
 
   # return name of specified dataset
   def name(self, d):
-    name, rc = runproc(self.exe + " --name " + str(d), getstdout=True)
+    name, rc = runproc(self.exe + " --name " + str(d), getstdout=True, verbose=self.verbose)
     if rc == 0:
       return name.rstrip()
 
   # return the latest dataset id, or None
   def latest(self):
-    dset, rc = runproc(self.exe + " --latest", getstdout=True)
+    dset, rc = runproc(self.exe + " --latest", getstdout=True, verbose=self.verbose)
     if rc == 0:
       return dset.rstrip()
 
   # return the location string for the given dataset id
   def location(self, d):
-    dset, rc = runproc(self.exe + " --location " + str(d), getstdout=True)
+    dset, rc = runproc(self.exe + " --location " + str(d), getstdout=True, verbose=self.verbose)
     if rc == 0:
       return dset.rstrip()
 
   # resume a transfer of specified dataset id, used in poststage
   def resume(self, d):
-    dset, rc = runproc(self.exe + " --resume --name " + str(d))[1]
+    dset, rc = runproc(self.exe + " --resume --name " + str(d), verbose=self.verbose)[1]
     return (rc == 0)
 
   # create summary file for specified dataset id, used in prestage
   def write_summary(self, d):
-    dset, rc = runproc(self.exe + " --summary --name " + str(d))[1]
+    dset, rc = runproc(self.exe + " --summary --name " + str(d), verbose=self.verbose)[1]
     return (rc == 0)
