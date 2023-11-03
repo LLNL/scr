4bb73e9e05f97d7d5dd0792a3bd2afb3cada4126
Adam Moody <moody20@llnl.gov>
2023-11-03 10:56:18 -0700

    scripts: forgot to pass new down nodes param to nodetests

--- scripts/python/scrjob/list_down_nodes.py
+++ scripts/python/scrjob/list_down_nodes.py
@@ -9,24 +9,24 @@ def list_down_nodes(jobenv,
 
     Requires a valid jobenv object.
 
-    The nodes to be checked should be given in nodes. If nodes is not
-    given, the jobenv will be queried to get the list of nodes in the
-    allocation.
+    The list of nodes to check can be given in nodes. If not given, the
+    jobenv is queried to get the list of nodes in the allocation.
 
-    Any nodes specified in nodes_down will be considered to be down
-    without checking. This is useful to keep a node down that was
-    reported as down earlier, but have since come back online.
+    Any nodes specified in nodes_down are considered to be down without
+    checking. This is useful to keep a node down that was earlier
+    reported as down but has since come back online.
 
-    The free parameter is not currently in use. Its intent is to direct
-    the storage capacity test to check free space vs total capacity.
+    The free parameter is not currently used. Its intent is to configure
+    the storage capacity test to check free drive space vs total
+    capacity.
 
-    If given a log object, logs nodes as NODE_FAIL. One can then
-    additionally specify the number of seconds since the run started in
-    secs.
+    If given a log object, appends NODE_FAIL messages to the log. One
+    can specify the number of seconds since the run started in secs.
 
-    If reason=False, returns a list of node names. If reason=True,
-    returns a dictionary, where the node name is the key and the reason
-    the node is determined to be down is the value.
+    If reason=False, list_down_nodes returns a list of node names. If
+    reason=True, list_down_nodes returns a dictionary, where the key is
+    the node name and the value is a string giving the reason that the
+    node is determined to be down.
     """
 
     # check that we have a list of nodes before going any further
@@ -39,7 +39,7 @@ def list_down_nodes(jobenv,
 
     # get a dictionary of all unavailable (down or excluded) nodes and reason
     # keys are nodes and the values are the reasons
-    unavailable = jobenv.nodetests.execute(nodes, jobenv)
+    unavailable = jobenv.nodetests.execute(nodes, jobenv, down=nodes_down)
 
     # log each newly failed node, along with the reason
     if log:
