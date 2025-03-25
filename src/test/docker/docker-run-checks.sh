2192c93a7239bd91e674e928ee48d03e9c781558
James Corbett <corbett8@llnl.gov>
2025-03-25 13:29:51 -0700

    docker: drop Flux-specific logic
    Problem: the docker infrastructure from flux-core has a lot of
    Flux-specific logic not needed here.
    
    Remove it.

--- src/test/docker/docker-run-checks.sh
+++ src/test/docker/docker-run-checks.sh
@@ -11,19 +11,20 @@ PROJECT=scr
 BASE_DOCKER_REPO=libscr/scr
 
 WORKDIR=/usr/src
-IMAGE=bookworm
+IMAGE=el9
 JOBS=2
 MOUNT_HOME_ARGS="--volume=$HOME:$HOME -e HOME"
+BUILD_DIR=build
 
 #
 declare -r prog=${0##*/}
 die() { echo -e "$prog: $@"; exit 1; }
 
 #
-declare -r long_opts="help,quiet,interactive,image:,flux-security-version:,jobs:,no-cache,no-home,distcheck,tag:,build-directory:,install-only,no-poison,recheck,unit-test-only,quick-check,inception,platform:,workdir:,system"
-declare -r short_opts="hqIdi:S:j:t:D:Prup:"
+declare -r long_opts="help,quiet,interactive,image:,jobs:,no-cache,no-home,tag:,build-directory:,install-only,recheck,unit-test-only,quick-check,platform:,workdir:,system"
+declare -r short_opts="hqIi:S:j:t:D:Prup:"
 declare usage="
-Usage: $prog [OPTIONS] -- [CONFIGURE_ARGS...]\n\
+Usage: $prog [OPTIONS] -- [CMAKE_ARGS...]\n\
 Build docker image for CI builds, then run tests inside the new\n\
 container as the current user and group.\n\
 \n\
@@ -34,19 +35,14 @@ Options:\n\
      --no-cache                Disable docker caching\n\
      --no-home                 Skip mounting the host home directory\n\
      --install-only            Skip make check, only make install\n\
-     --inception               Run tests as flux jobs\n\
-     --system                  Run under system instance\n\
  -q, --quiet                   Add --quiet to docker-build\n\
  -t, --tag=TAG                 If checks succeed, tag image as NAME\n\
  -i, --image=NAME              Use base docker image NAME (default=$IMAGE)\n\
  -p, --platform=NAME           Run on alternate platform (if supported)\n\
- -S, --flux-security-version=N Install flux-security vers N (default=$FLUX_SECURITY_VERSION)\n
  -j, --jobs=N                  Value for make -j (default=$JOBS)\n
- -d, --distcheck               Run 'make distcheck' instead of 'make check'\n\
  -r, --recheck                 Run 'make recheck' after failure\n\
  -u, --unit-test-only          Only run unit tests\n\
      --quick-check             Only run 'make check TESTS=' and one basic test\n\
- -P, --no-poison               Do not install poison libflux and flux(1)\n\
  -D, --build-directory=DIRNAME Name of a subdir to build in, will be made\n\
      --workdir=PATH            Use PATH as working directory for build\n\
  -I, --interactive             Instead of running ci build, run docker\n\
@@ -75,10 +71,8 @@ while true; do
       -q|--quiet)                  QUIET="--quiet";            shift   ;;
       -i|--image)                  IMAGE="$2";                 shift 2 ;;
       -p|--platform)               PLATFORM="--platform=$2";   shift 2 ;;
-      -S|--flux-security-version)  FLUX_SECURITY_VERSION="$2"; shift 2 ;;
       -j|--jobs)                   JOBS="$2";                  shift 2 ;;
       -I|--interactive)            INTERACTIVE="/bin/bash";    shift   ;;
-      -d|--distcheck)              DISTCHECK=t;                shift   ;;
       -r|--recheck)                RECHECK=t;                  shift   ;;
       -u|--unit-test-only)         UNIT_TEST_ONLY=t;           shift   ;;
       --quick-check)               QUICK_CHECK=t;              shift   ;;
@@ -88,9 +82,6 @@ while true; do
       --no-cache)                  NO_CACHE="--no-cache";      shift   ;;
       --no-home)                   MOUNT_HOME_ARGS="";         shift   ;;
       --install-only)              INSTALL_ONLY=t;             shift   ;;
-      --inception)                 INCEPTION=t;                shift   ;;
-      --system)                    SYSTEM=t;                   shift   ;;
-      -P|--no-poison)              POISON=0;                   shift   ;;
       -t|--tag)                    TAG="$2";                   shift 2 ;;
       --)                          shift; break;                       ;;
       *)                           die "Invalid option '$1'\n$usage"   ;;
@@ -108,36 +99,10 @@ else
 fi
 DOCKER_BUILD="docker build"
 
-# distcheck incompatible with some configure args
-if test "$DISTCHECK" = "t"; then
-    test "$RECHECK" = "t" && die "--recheck not allowed with --distcheck"
-    test "$SYSTEM" = "t" && die "--system not allowed with --distcheck"
-    for arg in "$@"; do
-        case $arg in
-          --sysconfdir=*|systemdsystemunitdir=*)
-            die "distcheck incompatible with configure arg $arg"
-        esac
-    done
-fi
-
-if test "$SYSTEM" = "t"; then
-    if test "$IMAGE" != "el8"; then
-        echo >&2 "Setting image to el8 for system checks build"
-    fi
-    IMAGE=el8
-    TAG="checks-builder:el8"
-    INSTALL_ONLY=t
-    NO_CACHE="--no-cache"
-    POISON=0
-fi
-
-CONFIGURE_ARGS="$@"
+CMAKE_ARGS="-DSCR_RESOURCE_MANAGER=NONE -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Debug $@ .."
 
 . ${TOP}/src/test/checks-lib.sh
 
-#  NOTE: BASE_IMAGE, IMAGESRC, FLUX_SECURITY_VERSION are ignored
-#   unless in flux-core repo
-#
 BUILD_IMAGE=${PROJECT}-checks-builder:${IMAGE}
 if test "$PROJECT" = "scr"; then
     DOCKERFILE=$TOP/src/test/docker/checks
@@ -155,7 +120,6 @@ checks_group "Building image $IMAGE for user $USER $(id -u) group=$(id -g)" \
     --build-arg USER=$USER \
     --build-arg UID=$(id -u) \
     --build-arg GID=$(id -g) \
-    --build-arg FLUX_SECURITY_VERSION=$FLUX_SECURITY_VERSION \
     ${BUILD_ARG:- } \
     -t ${BUILD_IMAGE} \
     ${DOCKERFILE} \
@@ -168,15 +132,11 @@ echo "mounting $TOP as $WORKDIR"
 
 export PLATFORM
 export PROJECT
-export POISON
-export INCEPTION
 export JOBS
-export DISTCHECK
 export RECHECK
 export UNIT_TEST_ONLY
 export QUICK_CHECK
 export BUILD_DIR
-export COVERAGE
 export chain_lint
 
 if [[ "$INSTALL_ONLY" == "t" ]]; then
@@ -188,8 +148,7 @@ if [[ "$INSTALL_ONLY" == "t" ]]; then
         sh -c "./autogen.sh &&
                ./configure --prefix=/usr --sysconfdir=/etc \
                 --with-systemdsystemunitdir=/etc/systemd/system \
-                --localstatedir=/var \
-                --with-flux-security &&
+                --localstatedir=/var &&
                make clean &&
                make -j${JOBS}"
     RC=$?
@@ -209,12 +168,8 @@ else
         -e CFLAGS \
         -e CPPFLAGS \
         -e GCOV \
-        -e CCACHE_CPP2 \
-        -e CCACHE_READONLY \
-        -e COVERAGE \
         -e TEST_INSTALL \
         -e CPPCHECK \
-        -e DISTCHECK \
         -e RECHECK \
         -e UNIT_TEST_ONLY \
         -e QUICK_CHECK \
@@ -223,28 +178,16 @@ else
         -e USER \
 	-e PROJECT \
         -e CI \
-        -e TAP_DRIVER_QUIET \
-        -e FLUX_TEST_TIMEOUT \
-        -e FLUX_TEST_SIZE_MAX \
         -e PYTHON \
         -e PYTHON_VERSION \
         -e PRELOAD \
-        -e POISON \
-        -e INCEPTION \
-        -e ASAN_OPTIONS \
         -e BUILD_DIR \
-        -e S3_ACCESS_KEY_ID \
-        -e S3_SECRET_ACCESS_KEY \
-        -e S3_HOSTNAME \
-        -e S3_BUCKET \
-	-e PSM3_HAL \
-	-e PSM3_DEVICES \
         --cap-add SYS_PTRACE \
         --tty \
         ${INTERACTIVE:+--interactive} \
         --network=host \
         ${BUILD_IMAGE} \
-        ${INTERACTIVE:-./src/test/checks_run.sh ${CONFIGURE_ARGS}} \
+        ${INTERACTIVE:-./src/test/checks_run.sh ${CMAKE_ARGS}} \
     || die "docker run failed"
 fi
 
@@ -260,21 +203,8 @@ if test -n "$TAG"; then
 	sh -c "make install && \
                userdel $USER" \
 	|| (docker rm tmp.$$; die "docker run of 'make install' failed")
-    docker commit \
-	--change 'ENTRYPOINT [ "/usr/local/sbin/entrypoint.sh" ]' \
-	--change 'CMD [ "/usr/bin/flux",  "start", "/bin/bash" ]' \
-	--change 'USER fluxuser' \
-	--change 'WORKDIR /home/fluxuser' \
-	tmp.$$ $TAG \
+    docker commit tmp.$$ $TAG \
 	|| die "docker commit failed"
     docker rm tmp.$$
     echo "Tagged image $TAG"
-fi
-
-if test -n "$SYSTEM"; then
-    ${TOP}/src/test/docker/docker-run-systest.sh \
-	--image=${TAG} \
-        --jobs=${JOBS} \
-        -- src/test/checks_run.sh ${CONFIGURE_ARGS} \
-    || die "docker-run-systest.sh failed"
 fi
